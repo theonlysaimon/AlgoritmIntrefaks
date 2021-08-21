dbacb2beef66b4ec65452c2145f45e41e643c44f
theonlysaimon <89244188+theonlysaimon@users.noreply.github.com>
2021-08-22 00:01:37 +0400

    New 0:00

--- keywords.py
+++ keywords.py
@@ -3,6 +3,8 @@ from pymystem3 import Mystem
 import string
 import os 
 import numpy as np
+from nltk import FreqDist
+import re
 
 while exit:
 #загрузка dataset
@@ -24,10 +26,7 @@ while exit:
         rus_stops = [word.strip() for word in stop_file.readlines()] 
 
     extended_punctuation = string.punctuation + '—»«...'
-    moi_analizator = Mystem()
-
     def passed_filter (some_word, stoplist):
-        import re
         some_word = some_word.strip()
         if some_word in extended_punctuation:
             return False
@@ -37,8 +36,9 @@ while exit:
             return False
         return True
 
+    moi_analizator = Mystem()
     def keywords_most_frequent_with_stop_and_lemm (some_text, num_most_freq, stoplist):
-        from nltk import FreqDist
+        
         lemmatized_text = [word for word in moi_analizator.lemmatize(some_text.lower()) 
                         if passed_filter(word, stoplist)]
         return [word_freq_pair[0] for word_freq_pair in FreqDist(lemmatized_text).most_common(num_most_freq)]
